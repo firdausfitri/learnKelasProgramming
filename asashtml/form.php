7b77378e2afe93c2f8c9d54f4fdb676eb86e0432
Firdaus Fitri <dausfitri@DausFitri-MBP.local>
2023-03-22 21:30:36 +0800

    form and globals php

---
+++ asashtml/form.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FORM</title>
+</head>
+<body>
+    <form action="globals.php" method="POST">
+        <p>
+        <label for="nama">Name</label>
+        <input type="text" name="nama">
+        </p>
+        <p>
+        <label for="umur">Age</label>
+        <input type="text" name="umur">
+        </p>
+        <button type="submit">Send</button>
+    </form>
+</body>
+</html>
